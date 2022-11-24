eb13377b7153f55b48eef7f40d1b2dd2f971d380
TsangHans <gzzenghan@189.cn>
2022-11-24 23:13:11 +0800

    update "application retire_exp"

--- application/retire_exp.py
+++ application/retire_exp.py
@@ -1,42 +1,39 @@
-import asyncio
+import time
 
-from machine_ddt import DDTAgent, Event, InGameData, InChallengeRoomData
+from machine_ddt import DDTAgent, Event, InGameData, InChallengeRoomData, DDTContextResource
 from machine_ddt.data import InEntryData, InArenaHallData
 
 ddt = DDTAgent()
-ddt.init()
 
 
-@ddt.coro_subscribe(Event.RoomToGame)
-async def exit_game(data: InGameData):
+@ddt.subscribe(Event.RoomToGame)
+def exit_game(data: InGameData, ctx: DDTContextResource):
     """定时退出游戏"""
-    await asyncio.sleep(120)
+    time.sleep(120)
     data.builtin_act.exit_game()
 
 
 @ddt.subscribe(Event.InGame)
-def in_game_cancel_host(data: InGameData):
+def in_game_cancel_host(data: InGameData, ctx: DDTContextResource):
     """取消托管"""
     if data.builtin_obs.host:
         data.builtin_act.cancel_host()
 
 
 @ddt.subscribe(Event.InChallengeRoom)
-def challenge_room_action(data: InChallengeRoomData):
+def challenge_room_action(data: InChallengeRoomData, ctx: DDTContextResource):
     """快速开始挑战赛"""
     data.builtin_act.quick_start()
 
 
 @ddt.subscribe(Event.InEntry)
-def in_entry_action(data: InEntryData):
+def in_entry_action(data: InEntryData, ctx: DDTContextResource):
     """开启挑战赛房间"""
+
     data.builtin_act.open_challenge_room()
 
 
 @ddt.subscribe(Event.InArenaHall)
-def close_arena_window_action(data: InArenaHallData):
+def close_arena_window_action(data: InArenaHallData, ctx: DDTContextResource):
     """关闭竞技大厅窗口"""
     data.builtin_act.close_arena_hall()
-
-
-ddt.run()
--- run.py
+++ run.py
@@ -1,51 +1,10 @@
-from machine_ddt import DDTAgent, Event, InRoomData, InGameData, RoomToGameData, InChallengeRoomData
-from machine_ddt.data import InEntryData, InArenaHallData
+from machine_ddt import DDTAgent, use_application
 
 ddt = DDTAgent()
 ddt.init()
 
 
-# @ddt.coro_subscribe(Event.InRoom)
-# async def prepare_coro(data: InRoomData):
-#     pass
-
-
-# @ddt.subscribe(Event.InRoom)
-# def prepare(data: InRoomData):
-#     pass
-
-
-@ddt.subscribe(Event.InGame)
-def game_policy(data: InGameData):
-    if data.builtin_obs.my_turn:
-        data.builtin_act.turn_right()
-        data.builtin_act.shoot(58.5)
-    # print("wind: ", data.builtin_obs.wind)
-    # print("angle: ", data.builtin_obs.angle)
-    # if data.builtin_obs.host:
-    #     data.builtin_act.cancel_host()
-    # if data.builtin_obs.my_turn:
-    #     data.builtin_act.turn_right()
-    #     data.builtin_act.shoot(50)
-
-
-# @ddt.subscribe(Event.RoomToGame)
-# def some_policy(data: RoomToGameData):
-#     data.builtin_act.retire_exp()
-
-@ddt.subscribe(Event.InChallengeRoom)
-def challenge_room_action(data: InChallengeRoomData):
-    data.builtin_act.open_setting()
-
-
-@ddt.subscribe(Event.InEntry)
-def in_entry_action(data: InEntryData):
-    pass
-
-
-@ddt.subscribe(Event.InArenaHall)
-def in_entry_action(data: InArenaHallData):
-    data.builtin_act.close_arena_hall()
+use_application("application", "retire_exp")
 
 
 ddt.run()
